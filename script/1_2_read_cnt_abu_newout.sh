cacdc658d5628787a23c05308697d9f008f501a7
hxq5421 <99377871+lvc5421@users.noreply.github.com>
2023-12-26 12:24:04 +0800

    Update 1_2_read_cnt_abu_newout.sh

--- script/1_2_read_cnt_abu_newout.sh
+++ script/1_2_read_cnt_abu_newout.sh
@@ -26,7 +26,7 @@ while read -r srr; do
         awk -F '[\t ]' '{print $2"\t"$3}' $name>$path_out_new/${srr}.out
         # echo "awk -F '[\t ]' '{print $2"\t"$3}' $name>$out_file/${srr}.out"
         grep "s__" $rep |awk -F '[| \t]' '{print$7"_"$8"\t"$9}'   >$path_abu/${srr}_abundance.txt 
-        # echo "grep "s__" $rep |awk -F '[| \t]' '{print$7"_"$8"\t"$9}'   >$path_abu/${srr}_abundance.txt"
+        # echo "grep "s__" $rep |awk -F '[| \t]' '{print$(NF-2)"_"$(NF-1)"\t"$NF}'   >$path_abu/${srr}_abundance.txt"
     else
         if [ -s $name ]; then
             rm $name
