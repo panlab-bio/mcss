6a92b13c342417e03da2c84dd98b4d9d806d1efb
hxq5421 <99377871+lvc5421@users.noreply.github.com>
2023-12-27 09:21:18 +0800

    Update strain_download.sh

--- tools/strain_download.sh
+++ tools/strain_download.sh
@@ -1,5 +1,5 @@
 cd data
-wget -O strain_download.tar.gz 'http://storage.live.com/items/B2A5926AB0601A6E!3084:/strain_download.tar.gz?authkey=!AIA94oJyoNOd104'
+wget -O strain_download.tar.gz http://storage.live.com/items/B2A5926AB0601A6E!3084:/strain_download.tar.gz?authkey=!AIA94oJyoNOd104
 if [ -f strain_download.tar.gz ]; then
 	tar xvzf strain_download.tar.gz
 fi
