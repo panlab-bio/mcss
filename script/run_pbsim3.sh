374013da5c638ba882ae1c5c619b73a4bf061511
hxq5421 <99377871+lvc5421@users.noreply.github.com>
2024-01-29 19:52:50 +0800

    Update run_pbsim3.sh

--- script/run_pbsim3.sh
+++ script/run_pbsim3.sh
@@ -1,3 +1,4 @@
+# call pbsim3
 path_out=$1
 method=$2
 model=$3
@@ -6,19 +7,16 @@ genome=$5
 prefix=$6
 pass_num=$7
 sample_n=$8
+flag_css=$9
 
 sim_res=sim_res_${sample_n}
 sim_concat=sim_concat_${sample_n}
-# sim_strain=sim_strain${sample_n}
+
 if [ ! -d $path_out/$sim_res ];then
     echo "mkdir $path_out/$sim_res"
     mkdir -p $path_out/$sim_res
 fi
 
-# if [ ! -d $path_out/$sim_strain ];then
-#     echo "mkdir $path_out/$sim_strain"
-#     mkdir -p $path_out/$sim_strain
-# fi
 
 if [ ! -d $path_out/$sim_concat ];then
     echo "mkdir $path_out/$sim_concat"
@@ -30,22 +28,27 @@ if [ ! -f $path_out/$sim_concat/sim.fastq ];then
     > $path_out/$sim_concat/sim.fastq
 fi
 
-# echo $genome
+
 genome_name=$(basename $genome)
-# echo $genome_name
-# genome_new=$path_out/$sim_strain/$genome_name
+
 genome_new=$genome
-# echo "gzip -dc ${genome}.gz > $genome_new"
-# gzip -dc ${genome}.gz > $genome_new
+
 
 cd $path_out/$sim_res
 
 echo "pbsim --strategy wgs --method $method --$method $model --depth $depth --genome $genome_new --prefix $prefix"
 
-pbsim --strategy wgs --method $method --$method $model --depth $depth --genome $genome_new --prefix $prefix --pass-num $pass_num
 
-samtools view -bS ${prefix}_0001.sam -o ${prefix}_0001.subreads.bam
-ccs ${prefix}_0001.subreads.bam ${prefix}_0001.ccs.fastq.gz
-gunzip ${prefix}_0001.ccs.fastq.gz
-cat ${prefix}_0001.ccs.fastq >> $path_out/$sim_concat/sim.fastq
+
+if [ $flag_css == "hifi" ]; then
+    pbsim --strategy wgs --method $method --$method $model --depth $depth --genome $genome_new --prefix $prefix --pass-num $pass_num
+    samtools view -bS ${prefix}_0001.sam -o ${prefix}_0001.subreads.bam
+    ccs ${prefix}_0001.subreads.bam ${prefix}_0001.ccs.fastq.gz
+    gunzip ${prefix}_0001.ccs.fastq.gz
+    cat ${prefix}_0001.ccs.fastq >> $path_out/$sim_concat/sim.fastq
+else
+    pbsim --strategy wgs --method $method --$method $model --depth $depth --genome $genome_new --prefix $prefix
+    cat ${prefix}_0001.fastq >> $path_out/$sim_concat/sim.fastq
+fi
+
 
